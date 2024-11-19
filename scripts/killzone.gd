0061af1cad14add9c6594e1a0ec97e8bed295f07
latiwagner <latiphyver.b@gmail.com>
2024-11-19 15:30:59 -0500

    devlog

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,10 +1,5 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
-
 func _on_body_entered(_body):
 	print("You Died!")
-	timer.start()
-	
-func _on_timer_timeout():
 	get_tree().reload_current_scene()
